cbe848de9976ed83860ed622a8499cd7b63a1381
Carlos Moyano <carlosmoyanor@gmail.com>
2019-09-23 15:54:44 -0400

    updated Readme

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_D7x6fs9SElgRAW8Y5WViljLV00HwqeN4tg';
 
   const onToken = token => {
     console.log(token);
