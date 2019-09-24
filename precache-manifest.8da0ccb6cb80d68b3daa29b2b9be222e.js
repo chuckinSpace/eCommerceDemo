e1ebab2c81ab6a249d6c029d2110edd9bb72d4ff
Carlos Moyano <carlosmoyanor@gmail.com>
2019-09-24 15:54:33 -0400

    Updates

---
+++ precache-manifest.8da0ccb6cb80d68b3daa29b2b9be222e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9055d32ea1d20ab6c150a8bfda3e7d9",
+    "url": "/eCommerceDemo/index.html"
+  },
+  {
+    "revision": "760684adc21d5de7b0e2",
+    "url": "/eCommerceDemo/static/css/main.13820d97.chunk.css"
+  },
+  {
+    "revision": "cf9f530d85c97b749957",
+    "url": "/eCommerceDemo/static/js/2.b0594ed9.chunk.js"
+  },
+  {
+    "revision": "760684adc21d5de7b0e2",
+    "url": "/eCommerceDemo/static/js/main.0b6a07e7.chunk.js"
+  },
+  {
+    "revision": "e10186816b5473f72e03",
+    "url": "/eCommerceDemo/static/js/runtime~main.821d25fa.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/eCommerceDemo/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/eCommerceDemo/static/media/shopping-bag.30f07071.svg"
+  }
+]);
